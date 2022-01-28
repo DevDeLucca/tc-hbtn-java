417a989c4114876bfd20e5a6fdd645d7db685e2a
Lucas Evangelista <73043760+DevDeLucca@users.noreply.github.com>
2022-01-28 12:09:11 -0300

    task

--- 0x01/somar/Numero.java
+++ 0x01/somar/Numero.java
@@ -1,7 +1,6 @@
-public class Program {
-    public static void main(String[] args) {
-        System.out.printf("%d\n", Numero.somar(24, 32));
-        System.out.printf("%d\n", Numero.somar(5, 0));
-        System.out.printf("%d\n", Numero.somar(4, -2));
+public class Numero {
+    public static int somar(int a, int b){
+        return a+b;
     }
+
 }
---
+++ 0x01/somar/Program.java
@@ -0,0 +1,7 @@
+public class Program {
+    public static void main(String[] args) {
+        System.out.printf("%d\n", Numero.somar(24, 32));
+        System.out.printf("%d\n", Numero.somar(5, 0));
+        System.out.printf("%d\n", Numero.somar(4, -2));
+    }
+}
