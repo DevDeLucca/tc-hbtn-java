e2fc8d4df8a579cfeb273817837696f99c4ae654
Lucas Evangelista <73043760+DevDeLucca@users.noreply.github.com>
2022-02-03 09:26:48 -0300

    Pacotes e herancas

--- 0x04/forma/Program.java
+++ 0x04/forma/Program.java
@@ -1,10 +1,10 @@
+
 public class Program {
     public static void main(String[] args) {
-        FormaGeometrica formaGeometrica = new FormaGeometica();
+        FormaGeometrica formaGeometrica = new FormaGeometrica();
 
         try {
             System.out.printf("Area: %d", formaGeometrica.area());
-
         } catch (Exception e) {
             System.out.println(e);
         }
