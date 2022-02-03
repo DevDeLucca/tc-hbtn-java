d4a942c1418633161102fc68290d6c0320c1992e
Lucas Evangelista <73043760+DevDeLucca@users.noreply.github.com>
2022-02-03 09:22:08 -0300

    Pacotes e herancas

--- 0x04/heranca/Program.java
+++ 0x04/heranca/Program.java
@@ -1,17 +1,16 @@
-public class program {
+public class Program {
 
-    public static void main(String [] args){
+    public static void main(String[] args) {
 
-        Animal meuAnima = new Animal();
-        Cachorro meuCachorro = new Cachorro)();
-        object obj = meuCachorro;
-        Animal umOutroAnimal = meCachorro;
+        Animal meuAnimal = new Animal();
+        Cachorro meuCachorro = new Cachorro();
+        Object obj = meuCachorro;
+        Animal umOutroAnimal = meuCachorro;
 
         System.out.println("meuAnimal eh um " + meuAnimal.getClass().getName());
         System.out.println("meuCachorro eh um " + meuCachorro.getClass().getName());
         System.out.println("obj eh um " + obj.getClass().getName());
         System.out.println("umOutroAnimal eh um " + umOutroAnimal.getClass().getName());
-
-        }
+    }
 
 }
