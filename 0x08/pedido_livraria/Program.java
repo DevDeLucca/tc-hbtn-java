f9ddf1631fc9cb7c4a201d3a5a8dbb19a3d99e84
Lucas Evangelista <73043760+DevDeLucca@users.noreply.github.com>
2022-02-04 13:24:22 -0300

    Pedido Livraria-Polimorfismo

--- 0x08/pedido_livraria/Program.java
+++ 0x08/pedido_livraria/Program.java
@@ -1,22 +1,35 @@
-public class Program{
-    public class static void main(String [] args){
-        produto.Livro1 livro1= new produto.Livro("Duna", 2017, "Brasil", 56.30, 680, "Frank Herbert", 1);
-        produto.Livro2 livro2= new produto.Livro("Um do nós esta mentindo", 2018, "Brasil", 34.93, 384,
-                "Karem Mcmanus, 1");
-        produto.Livro3 livro3= new produto.Livro("Mindset Milionário", 2021, "Brasil", 31.70, 272,
-                "José Roberto Marques" 1);
-        produto.Dvd1 dvd1= new produto.Dvd("Anjos da noite 5 - Guerra de sangue", 2016, "Estados Unidos", 16.90,
-                "Anna Foerster", "Ação", 91);
-        produto.Dvd2 dvd2= new produto.Dvd("Annabelle 2 - A criação do Mal", 2017, "Brasil", 39.90,"James Wan e Peter Safran",
-                "Terror", 109);
+import java.util.Locale;
 
-        //Pedido 1
-        ItemPedido[] itemPedidos1 = new ItemPedido[]{
+import produtos.Produto;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(new Locale("pt", "BR"));
+
+        produtos.Livro livro1 = new produtos.Livro("Duna", 2017, "Brasil", 56.30, 680, "Frank Herbert", 1);
+        produtos.Livro livro2 = new produtos.Livro("Um de nós está mentindo", 2018, "Brasil", 34.93, 384,
+                "Karen Mcmanus", 1);
+        produtos.Livro livro3 = new produtos.Livro("Mindset Milionário", 2021, "Brasil", 31.70, 272,
+                "José Roberto Marques", 1);
+
+        produtos.Dvd dvd1 = new produtos.Dvd("Anjos da Noite 5 - Guerras de Sangue", 2016, "Estados Unidos", 16.90,
+                "Anna Foerster",
+                "Ação", 91);
+        produtos.Dvd dvd2 = new produtos.Dvd("Annabelle 2 - A Criação do Mal", 2017, "Brasil", 39.90,
+                "Jame Wan e Peter Safran",
+                "Terror",
+                109);
+
+        // Pedido 1
+        ItemPedido[] itensPedido1 = new ItemPedido[] {
                 new ItemPedido(livro1, 1),
-                new ItemPedido(Dvd2, 1)
+                new ItemPedido(dvd2, 1)
         };
-        Pedido pedido1 = new Pedido(0, itemPedidos1);
+
+        Pedido pedido1 = new Pedido(0, itensPedido1);
+
         System.out.printf("Total Pedido: %.2f\n", pedido1.calcularTotal());
+
 
         // Pedido 2
         ItemPedido[] itensPedido2 = new ItemPedido[] {
@@ -27,6 +40,7 @@ public class Program{
         Pedido pedido2 = new Pedido(10, itensPedido2);
 
         System.out.printf("Total Pedido: %.2f\n", pedido2.calcularTotal());
+
 
         // Pedido 3
         ItemPedido[] itensPedido3 = new ItemPedido[] {
@@ -51,6 +65,6 @@ public class Program{
         Pedido pedido4 = new Pedido(5, itensPedido4);
 
         System.out.printf("Total Pedido: %.2f\n", pedido4.calcularTotal());
+
     }
 }
-
