c8947ea6cfb080880aa12482e079b31beeb63b79
Lucas Evangelista <73043760+DevDeLucca@users.noreply.github.com>
2022-02-10 11:47:21 -0300

    Collections ArrayList

--- 0x10/todo/Program.java
+++ 0x10/todo/Program.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class ListaTodo public class Program {
+public class ListaTodo {
     public static void main(String[] args) {
         ListaTodo todo = new ListaTodo();
 
