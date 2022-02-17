cae4e34ac20e2c916e91cbcf9482a26179fb197c
Lucas Evangelista <73043760+DevDeLucca@users.noreply.github.com>
2022-02-17 10:57:38 -0300

    Java - Interfaces atividades

---
+++ 0x13/workflow/atividades/CarregarVideo.java
@@ -0,0 +1,14 @@
+package atividades;
+public class CarregarVideo implements Atividade{
+
+    public CarregarVideo() {
+        executar();
+    }
+
+    @Override
+    public void executar() {
+        System.out.println("carregando video");
+    }
+
+
+}
