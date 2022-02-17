df37ee751cfd823c47ce441de1fc9720485d289f
Lucas Evangelista <73043760+DevDeLucca@users.noreply.github.com>
2022-02-17 11:01:27 -0300

    Java - Interfaces atividades

---
+++ 0x13/workflow/atividades/CodificarVideo.java
@@ -0,0 +1,13 @@
+package atividades;
+public class CodificarVideo implements atividades {
+
+    public CodificarVideo() {
+        executar();
+    }
+
+    @Override
+    public void executar() {
+        System.out.println("codificando video");
+    }
+
+}
