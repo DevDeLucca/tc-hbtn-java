749257499270e1f7d3e236c8a37252aafbebf8a3
Lucas Evangelista <73043760+DevDeLucca@users.noreply.github.com>
2022-02-21 22:45:41 -0300

    Java - Collections, Sets e Maps

--- 0x14/telefones_duplicados/ListaTelefonica.java
+++ 0x14/telefones_duplicados/ListaTelefonica.java
@@ -4,55 +4,55 @@ import java.util.Map.Entry;
 
 public class ListaTelefonica {
 
-    private HashMap<String, HashSet<Telefone>> listaTelefones;
+	private HashMap<String, HashSet<Telefone>> listaTelefones;
 
-    public ListaTelefonica() {
-        listaTelefones = new HashMap<>();
-    }
+	public ListaTelefonica() {
+		listaTelefones = new HashMap<>();
+	}
 
-    public HashSet<Telefone> adicionarTelefone(String nome, Telefone telefone) {
-        HashSet<Telefone> telTempLoc = new HashSet<Telefone>();
-        boolean achou = false;
-        String chave = "";
+	public HashSet<Telefone> adicionarTelefone(String nome, Telefone telefone) {
+		HashSet<Telefone> telTempLoc = new HashSet<Telefone>();
+		boolean achou = false;
+		String chave = "";
+		
+		
+		for (Entry<String, HashSet<Telefone>> entry : listaTelefones.entrySet()) {
+			if (entry.getValue().contains(telefone)) {
+				achou=true;
+				chave = entry.getKey();
+			}
+		}
+		
+		if(achou == false && listaTelefones.containsKey(nome)) {
+			telTempLoc.addAll(listaTelefones.get(nome));
+			telTempLoc.add(telefone);
+			listaTelefones.put(nome, telTempLoc);
+			
+		}
+		
+		if(chave.equals(nome) && achou) {
+			throw new IllegalArgumentException("Telefone jah existente para essa pessoa");
+		}else if(!chave.equals(nome) && achou){
+			throw new IllegalArgumentException("Telefone jah pertence a outra pessoa");
+		}
 
+		telTempLoc.add(telefone);
+		listaTelefones.put(nome, telTempLoc);
+		
+		return telTempLoc;
 
-        for (Entry<String, HashSet<Telefone>> entry : listaTelefones.entrySet()) {
-            if (entry.getValue().contains(telefone)) {
-                achou=true;
-                chave = entry.getKey();
-            }
-        }
+	}
 
-        if(achou == false && listaTelefones.containsKey(nome)) {
-            telTempLoc.addAll(listaTelefones.get(nome));
-            telTempLoc.add(telefone);
-            listaTelefones.put(nome, telTempLoc);
+	public HashSet<Telefone> buscar(String nome) {
 
-        }
+		HashSet<Telefone> telTemp = new HashSet<Telefone>();
 
-        if(chave.equals(nome) && achou) {
-            throw new IllegalArgumentException("Telefone jah existente para essa pessoa");
-        }else if(!chave.equals(nome) && achou){
-            throw new IllegalArgumentException("Telefone jah pertence a outra pessoa");
-        }
-
-        telTempLoc.add(telefone);
-        listaTelefones.put(nome, telTempLoc);
-
-        return telTempLoc;
-
-    }
-    public HashSet<Telefone> buscar(String nome) {
-
-        HashSet<Telefone> telTemp = new HashSet<Telefone>();
-
-        if (!listaTelefones.containsKey(nome)) {
-            return null;
-        } else {
-            telTemp = listaTelefones.get(nomes);
-            return telTemp;
-        }
-
-    }
+		if (!listaTelefones.containsKey(nome)) {
+			return null;
+		} else {
+			telTemp = listaTelefones.get(nome);
+			return telTemp;
+		}
+	}
 
 }
