859535427f5004c292b7b6ee65a1c0fbca62c8ed
Lucas Evangelista <73043760+DevDeLucca@users.noreply.github.com>
2022-02-23 10:23:02 -0300

    Java - Collections - Exercícios

--- 0x15/blog/Post.java
+++ 0x15/blog/Post.java
@@ -1,96 +1,96 @@
 public class Post {
 
-    private String autor;
-    private String titulo;
-    private String corpo;
-    private String categoria;
+	private String autor;
+	private String titulo;
+	private String corpo;
+	private String categoria;
 
-    public post (autor, titulo, corpo, categoria) {
-        super();
-        this.autor = autor;
-        this.titulo = titulo;
-        this.corpo = corpo;
-        this.categoria = categoria;
-    }
+	public Post(String autor, String titulo, String corpo, String categoria) {
+		super();
+		this.autor = autor;
+		this.titulo = titulo;
+		this.corpo = corpo;
+		this.categoria = categoria;
+	}
 
-    public String getAutor() {
-        return autor;
-    }
+	public String getAutor() {
+		return autor;
+	}
 
-    public void setAutor(String autor) {
-        this.autor = autor;
-    }
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
 
-    public String getCategoria() {
-        return categoria;
-    }
+	public String getTitulo() {
+		return titulo;
+	}
 
-    public void setCategoria(String categoria) {
-        this.categoria = categoria;
-    }
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
 
-    public String getCorpo() {
-        return corpo;
-    }
+	public String getCorpo() {
+		return corpo;
+	}
 
-    public void setCorpo(String corpo) {
-        this.corpo = corpo;
-    }
+	public void setCorpo(String corpo) {
+		this.corpo = corpo;
+	}
 
-    public String getTitulo() {
-        return titulo;
-    }
+	public String getCategoria() {
+		return categoria;
+	}
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
 
-    @Override
-    public String toString() {
-        return "Post [autor=" + autor + ", titulo=" + titulo + ", corpo=" + corpo + ", categoria=" + categoria + "]";
-    }
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 5;
-        result = prime * result + ((autor == null) ? 0 : autor.hashCode());
-        result = prime * result + ((categoria == null) ? 0 : categoria.hashCode());
-        result = prime * result + ((corpo == null) ? 0 : corpo.hashCode());
-        result = prime * result + ((titulo == null) ? 0 : titulo.hashCode());
-        return result;
-    }
+	@Override
+	public String toString() {
+		return "Post [autor=" + autor + ", titulo=" + titulo + ", corpo=" + corpo + ", categoria=" + categoria + "]";
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Post other = (Post) obj;
-        if (autor == null) {
-            if (other.autor != null)
-                return false;
-        } else if (!autor.equals(other.autor))
-            return false;
-        if (categoria == null) {
-            if (other.categoria != null)
-                return false;
-        } else if (!categoria.equals(other.categoria))
-            return false;
-        if (corpo == null) {
-            if (other.corpo != null)
-                return false;
-        } else if (!corpo.equals(other.corpo))
-            return false;
-        if (titulo == null) {
-            if (other.titulo != null)
-                return false;
-        } else if (!titulo.equals(other.titulo))
-            return false;
-        return true;
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 5;
+		result = prime * result + ((autor == null) ? 0 : autor.hashCode());
+		result = prime * result + ((categoria == null) ? 0 : categoria.hashCode());
+		result = prime * result + ((corpo == null) ? 0 : corpo.hashCode());
+		result = prime * result + ((titulo == null) ? 0 : titulo.hashCode());
+		return result;
+	}
 
-}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Post other = (Post) obj;
+		if (autor == null) {
+			if (other.autor != null)
+				return false;
+		} else if (!autor.equals(other.autor))
+			return false;
+		if (categoria == null) {
+			if (other.categoria != null)
+				return false;
+		} else if (!categoria.equals(other.categoria))
+			return false;
+		if (corpo == null) {
+			if (other.corpo != null)
+				return false;
+		} else if (!corpo.equals(other.corpo))
+			return false;
+		if (titulo == null) {
+			if (other.titulo != null)
+				return false;
+		} else if (!titulo.equals(other.titulo))
+			return false;
+		return true;
+	}
+
 }
