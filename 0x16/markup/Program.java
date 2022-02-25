683c56aa38d133e372b62f31b602f7085ca7ab67
Lucas Evangelista <73043760+DevDeLucca@users.noreply.github.com>
2022-02-25 10:27:35 -0300

    Java - Expressões Lambda

--- 0x16/markup/Program.java
+++ 0x16/markup/Program.java
@@ -1,16 +1,11 @@
-Program.java
 public class Program {
-    public static void main(String[] args) {
-        Produto produto1 = new Produto(129.99, "Mouse Gamer");
+	public static void main(String[] args) {
+		Produto produto1 = new Produto(129.99, "Mouse Gamer");
 
-        System.out.printf("%s - %.2f - %.2f\n", produto1.getNome(),
-                produto1.getPreco(),
-                produto1.precoComMarkUp.get());
+		System.out.printf("%s - %.2f - %.2f\n", produto1.getNome(), produto1.getPreco(), produto1.precoComMarkUp.get());
 
-        produto1.atualizarMarkUp.accept(15d);
+		produto1.atualizarMarkUp.accept(15d);
 
-        System.out.printf("%s - %.2f - %.2f\n", produto1.getNome(),
-                produto1.getPreco(),
-                produto1.precoComMarkUp.get());
-    }
+		System.out.printf("%s - %.2f - %.2f\n", produto1.getNome(), produto1.getPreco(), produto1.precoComMarkUp.get());
+	}
 }
