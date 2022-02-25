8ac02eb179338c312a46e3d17180b6a60bc4293a
Lucas Evangelista <73043760+DevDeLucca@users.noreply.github.com>
2022-02-25 12:03:18 -0300

    errata

--- 0x17/predicado/ConsultaProdutos.java
+++
@@ -1,9 +0,0 @@
-import java.util.stream.Collectors;
-import java.util.List;
-
-public class ConsultaProdutos {
-
-    public static List<Produto> filtrar (List<Produto> produtos, CriterioFiltro criterioFiltro){
-        return produtos.stream().filter(p -> criterioFiltro.testar(p)).collect(Collectors.toList());
-    }
-}
--- 0x17/predicado/TiposProduto.java
+++
@@ -1,3 +0,0 @@
-public enum TiposProduto  {
-    COZINHA, BRINQUEDO, ELETRONICO
-    }
