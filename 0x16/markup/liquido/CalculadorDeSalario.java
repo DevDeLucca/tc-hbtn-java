8ddf1cd688b3300a0f07d7ee1caea42869223370
Lucas Evangelista <73043760+DevDeLucca@users.noreply.github.com>
2022-02-25 10:23:30 -0300

    errata

--- 0x16/markup/liquido/CalculadorDeSalario.java
+++
@@ -1,4 +0,0 @@
-public class CalculadorDeSalario {
-
-    private double calcular(double d);
-}
--- 0x16/markup/liquido/FolhaPagamento.java
+++
@@ -1,6 +0,0 @@
-public class FolhaPagamento {
-
-    private static double calcularLiquido(Funcionario funcionario, CalculadorDeSalario calculadora) {
-        return calculadora.calcular(funcionario.getSalario());
-    }
-}
