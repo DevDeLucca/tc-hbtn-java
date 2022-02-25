2aa1f20dc7937657128d9582415a057400b70801
Lucas Evangelista <73043760+DevDeLucca@users.noreply.github.com>
2022-02-25 10:23:08 -0300

    Java - Expressões Lambda

---
+++ 0x16/markup/liquido/Program.java
@@ -0,0 +1,13 @@
+public class Program {
+    public static void main(String[] args) {
+        Funcionario funcionario1 = new Funcionario(2360);
+
+        double salarioLiquido90porcento = FolhaPagamento.calcularLiquido(funcionario1, x -> x * 0.9);
+        double salarioLiquidoAcrescimo50 = FolhaPagamento.calcularLiquido(funcionario1, x -> x + 50);
+        double salarioLiquidoAcrescimo10porcento = FolhaPagamento.calcularLiquido(funcionario1, x -> x * 1.1);
+
+        System.out.printf("%.2f - %.2f\n", funcionario1.getSalario(), salarioLiquido90porcento);
+        System.out.printf("%.2f - %.2f\n", funcionario1.getSalario(), salarioLiquidoAcrescimo50);
+        System.out.printf("%.2f - %.2f\n", funcionario1.getSalario(), salarioLiquidoAcrescimo10porcento);
+    }
+}
