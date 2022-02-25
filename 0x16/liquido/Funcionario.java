5a53b61479caeeefd5d38a28631ba23186177175
Lucas Evangelista <73043760+DevDeLucca@users.noreply.github.com>
2022-02-25 10:30:02 -0300

    Java - Expressões Lambda

--- 0x16/liquido/Funcionario.java
+++ 0x16/liquido/Funcionario.java
@@ -1,17 +1,23 @@
 public class Funcionario {
+	
+	private double salario;
 
-    private double salario;
+	public Funcionario() {
+	
+	}
 
-    public Funcionario(double salario) {
-        super();
-        this.salario = salario;
-    }
+	public Funcionario(double salario) {
+		super();
+		this.salario = salario;
+	}
 
-    public double getSalario() {
-        return salario;
-    }
+	public double getSalario() {
+		return salario;
+	}
 
-    public void setSalario(double salario) {
-        this.salario = salario;
-    }
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	
+
 }
