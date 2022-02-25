46513d41fe6ede2de531cee6189c5f465fe5d22c
Lucas Evangelista <73043760+DevDeLucca@users.noreply.github.com>
2022-02-25 10:24:58 -0300

    Java - Expressões Lambda

--- 0x16/markup/Produto.java
+++ 0x16/markup/Produto.java
@@ -1,53 +1,54 @@
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
 public class Produto {
 
-    public static void main (String [] args) {
+	private String nome;
+	private Double preco;
+	private Double percentualMarkUp = 0.1;
+	
+	public Produto() {
+		
+	}
+	
+	
+	public Produto(Double preco, String nome) {
+		super();
+		this.nome = nome;
+		this.preco = preco;
+		
+	}
 
-        private String nome;
-        private double preco;
-        private double percentualMarkUp = 0.1;
-
-        public Produto () {
-
-        }
-
-        public Produto(double preco, String nome) {
-            super();
-            this.preco = preco;
-            this.nome = nome;
-        }
-
-        Supplier<double> precoComMarkUp = () -> preco*(1-percentualMarkUp);
-        Consumer<double> atualizarMarkUp = valor ->  this.percentualMarkUp =(valor/100);
-
-        public String getNome() {
-            return nome;
-        }
-
-
-        public void setNome(String nome) {
-            this.nome = nome;
-        }
+	Supplier<Double> precoComMarkUp = () -> preco*(1+percentualMarkUp); 
+	Consumer<Double> atualizarMarkUp = valor ->  this.percentualMarkUp =(valor/100);
+	
+	
+	public String getNome() {
+		return nome;
+	}
 
 
-        public Double getPreco() {
-            return preco;
-        }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
 
-        public void setPreco(Double preco) {
-            this.preco = preco;
-        }
+	public Double getPreco() {
+		return preco;
+	}
 
 
-        public Double getPercentualMarkUp() {
-            return percentualMarkUp;
-        }
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
 
 
-        public void setPercentualMarkUp(Double percentualMarkUp) {
-            this.percentualMarkUp = percentualMarkUp;
-        }
+	public Double getPercentualMarkUp() {
+		return percentualMarkUp;
+	}
 
 
-    }
-}
+	public void setPercentualMarkUp(Double percentualMarkUp) {
+		this.percentualMarkUp = percentualMarkUp;
+	}
+	
