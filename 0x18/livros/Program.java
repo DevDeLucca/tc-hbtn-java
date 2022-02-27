9d88620cbcabfd8dc96ccef4662c0be6c2ab8285
Lucas Evangelista <73043760+DevDeLucca@users.noreply.github.com>
2022-02-27 00:01:13 -0300

    Java - Streams e Collectors

--- 0x18/livros/Program.java
+++ 0x18/livros/Program.java
@@ -1,24 +1,26 @@
 import java.util.List;
+import java.util.Locale;
 
 public class Program {
     public static void main(String[] args) {
+    	Locale.setDefault(new Locale("pt", "BR"));
         Produto produto1 = new Produto(1,
-                "Carro Vortex", CategoriaProduto.BRINQUEDO, 799.90);
+            "Carro Vortex", CategoriaProduto.BRINQUEDO, 799.90);
 
         Produto produto2 = new Produto(2,
-                "Smart TV LED 32 HD LG", CategoriaProduto.ELETRONICO, 1452.55);
+            "Smart TV LED 32 HD LG", CategoriaProduto.ELETRONICO, 1452.55);
 
         Produto produto3 = new Produto(3,
-                "Nintendo Switch", CategoriaProduto.ELETRONICO, 2990.00);
+            "Nintendo Switch", CategoriaProduto.ELETRONICO, 2990.00);
 
         Produto produto4 = new Produto(4,
-                "Ladrao de Raios", CategoriaProduto.LIVRO, 21.88);
+            "Ladrao de Raios", CategoriaProduto.LIVRO, 21.88);
 
         Produto produto5 = new Produto(5,
-                "Xato", CategoriaProduto.LIVRO, 38.30);
+            "Xato", CategoriaProduto.LIVRO, 38.30);
 
         Produto produto6 = new Produto(6,
-                "Coracoes sujos", CategoriaProduto.LIVRO, 27.50);
+            "Coracoes sujos", CategoriaProduto.LIVRO, 27.50);            
 
         Cliente cliente1 = new Cliente(1, "Jane Doe", false);
         Cliente cliente2 = new Cliente(2, "John Bannins", true);
