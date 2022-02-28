ad473b1d961dd13948713b6d4930c1df6ada31d9
Lucas Evangelista <73043760+DevDeLucca@users.noreply.github.com>
2022-02-28 14:29:34 -0300

    Update Cliente.java

--- 0x18/pedidos_eletronico/Cliente.java
+++ 0x18/pedidos_eletronico/Cliente.java
@@ -1,43 +1,43 @@
 public class Cliente {
 
-    private int codigo;
-    private String nome;
-    private boolean premium;
+	private int codigo;
+	private String nome;
+	private boolean premium;
+	
+	public Cliente() {
+		
+	}
 
-    public Cliente() {
+	public Cliente(int codigo, String nome, boolean premium) {
+		super();
+		this.codigo = codigo;
+		this.nome = nome;
+		this.premium = premium;
+	}
 
-    }
+	public int getCodigo() {
+		return codigo;
+	}
 
-    public Cliente(int codigo, String nome, boolean premium) {
-        super();
-        this.codigo = codigo;
-        this.nome = nome;
-        this.premium = premium;
-    }
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
 
-    public int getCodigo() {
-        return codigo;
-    }
+	public String getNome() {
+		return nome;
+	}
 
-    public void setCodigo(int codigo) {
-        this.codigo = codigo;
-    }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-    public String getNome() {
-        return nome;
-    }
+	public boolean isPremium() {
+		return premium;
+	}
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public boolean isPremium() {
-        return premium;
-    }
-
-    public void setPremium(boolean premium) {
-        this.premium = premium;
-    }
-
-
+	public void setPremium(boolean premium) {
+		this.premium = premium;
+	}
+	
+	
 }
