f31c5389d9374a46fc8e03b6bacb0baee5bb5ecc
Lucas Evangelista <73043760+DevDeLucca@users.noreply.github.com>
2022-02-28 13:56:59 -0300

    Java - Streams e Collectors

---
+++ 0x18/maior_preco/Consulta.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Consulta {
+
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+
+        List<Produto> produtosPedidos = new ArrayList<Produto>(pedido.getProdutos());
+
+        List<Produto> filter = produtosPedidos.stream().filter(p -> p.getCategoria().equals(CategoriaProduto.LIVRO))
+                .collect(Collectors.toList());
+
+        return filter;
+
+    }
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
+
+        return produtos.stream()
+                .max(Comparator.comparing(Produto::getPreco)).get();
+    }
+}
