4cb79e42356a3600e7d5d862f818479beead89d2
Lucas Evangelista <73043760+DevDeLucca@users.noreply.github.com>
2022-02-28 14:32:47 -0300

    Update Produto.java

--- 0x18/pedidos_eletronico/Produto.java
+++ 0x18/pedidos_eletronico/Produto.java
@@ -1,59 +1,59 @@
 public class Produto {
+	
+	private int codigo; 
+	private String nome;
+	private CategoriaProduto categoria; 
+	private double preco;
+	
+	public Produto() {
+		
+	}
 
-    private int codigo;
-    private String nome;
-    private CategoriaProduto categoria;
-    private double preco;
+	public Produto(int codigo, String nome, CategoriaProduto categoria, double preco) {
+		super();
+		this.codigo = codigo;
+		this.nome = nome;
+		this.categoria = categoria;
+		this.preco = preco;
+	}
 
-    public Produto() {
+	public int getCodigo() {
+		return codigo;
+	}
 
-    }
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
 
-    public Produto(int codigo, String nome, CategoriaProduto categoria, double preco) {
-        super();
-        this.codigo = codigo;
-        this.nome = nome;
-        this.categoria = categoria;
-        this.preco = preco;
-    }
+	public String getNome() {
+		return nome;
+	}
 
-    public int getCodigo() {
-        return codigo;
-    }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-    public void setCodigo(int codigo) {
-        this.codigo = codigo;
-    }
+	public CategoriaProduto getCategoria() {
+		return categoria;
+	}
 
-    public String getNome() {
-        return nome;
-    }
+	public void setCategoria(CategoriaProduto categoria) {
+		this.categoria = categoria;
+	}
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+	public double getPreco() {
+		return preco;
+	}
 
-    public CategoriaProduto getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(CategoriaProduto categoria) {
-        this.categoria = categoria;
-    }
-
-    public double getPreco() {
-        return preco;
-    }
-
-    public void setPreco(double preco) {
-        this.preco = preco;
-    }
-
-
-    @Override
-    public String toString() {
-        return "["+getCodigo()+"] "+ getNome()+" "+getCategoria()+
-                " R$ " +String.format("%.2f", getPreco());
-    }
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "["+getCodigo()+"] "+ getNome()+" "+getCategoria()+
+				" R$ " +String.format("%.2f", getPreco());
+	}
 
 }
