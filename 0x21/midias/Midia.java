359166fc404c22369551f7969628e1354765ac06
Lucas Evangelista <73043760+DevDeLucca@users.noreply.github.com>
2022-03-02 21:34:25 -0300

    Java - Generics

--- 0x21/midias/Midia.java
+++ 0x21/midias/Midia.java
@@ -1,26 +1,27 @@
-public class Midia {
+public abstract class Midia {
+	
+	private String nome;
+	
+	public Midia() {
+		
+	}
+	
+	public Midia(String nome) {
+		super();
+		this.nome = nome;
+	}
 
-    private String nome;
+	public String getNome() {
+		return nome;
+	}
 
-    public Midia() {
-    }
-
-    public Midia(String nome) {
-        super();
-        this.setNome(nome);
-    }
-
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("Tipo: %s - Nome: %s", this.getClass().getName(), getNome());
-    }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("Tipo: %s - Nome: %s", this.getClass().getName(), getNome());
+	}
+	
 }
