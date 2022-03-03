3c119460eaabc561fa451311c97a2e77996726d6
Lucas Evangelista <73043760+DevDeLucca@users.noreply.github.com>
2022-03-03 08:36:41 -0300

    Java - Generics - Exercícios

--- 0x22/armazem/Armazem.java
+++ 0x22/armazem/Armazem.java
@@ -1,22 +1,22 @@
-public class Armazem  implements Armazenavel {
+import java.util.Map;
+import java.util.TreeMap;
 
-    public class Armazem<T> implements armazenavel<T>();
+public class Armazem<T> implements Armazenavel<T> {
 
+	private Map<String, T> mapa = new TreeMap<>();
+	
 
-    private Map<String, T> mapa = new TreeMap<>();
+	@Override
+	public void adicionarAoInventario(String nome, T valor) {
+		mapa.put(nome, valor);
+		
+	}
 
-
-    @Override
-    public void adicionarAoInventario(String nome, T valor) {
-        mapa.put(nome, valor);
-
-    }
-
-    @Override
-    public T obterDoInventario(String nome) {
-
-
-        return mapa.get(nome);
-    }
+	@Override
+	public T obterDoInventario(String nome) {
+		
+		
+		return mapa.get(nome);
+	}
 
 }
