460c2dc75ea7f862ee62cc156e9c4e9dd4b90756
Lucas Evangelista <73043760+DevDeLucca@users.noreply.github.com>
2022-03-03 08:38:51 -0300

    Java - Generics - Exercícios

--- 0x22/armazem/Program.java
+++ 0x22/armazem/Program.java
@@ -1,28 +1,30 @@
 import java.util.*;
 
 public class Program {
-    public static void main(String[] args) {
-        Comida comida1 = new Comida("arroz", 100, 15.9);
-        Comida comida2 = new Comida("feijao", 80, 23.0);
-        Comida comida3 = new Comida("pacoca", 165, 7.5);
+	public static void main(String[] args) {
+		
+		Locale.setDefault(new Locale("pt", "BR"));
+		Comida comida1 = new Comida("arroz", 100, 15.9);
+		Comida comida2 = new Comida("feijao", 80, 23.0);
+		Comida comida3 = new Comida("pacoca", 165, 7.5);
 
-        Eletronico eletronico1 = new Eletronico("Smart TV LED 32 HD LG", 1452.55);
-        Eletronico eletronico2 = new Eletronico("Nintendo Switch", 2990);
+		Eletronico eletronico1 = new Eletronico("Smart TV LED 32 HD LG", 1452.55);
+		Eletronico eletronico2 = new Eletronico("Nintendo Switch", 2990);
 
-        Armazem<Eletronico> armazem1 = new Armazem<>();
-        armazem1.adicionarAoInventario("TV LG", eletronico1);
-        armazem1.adicionarAoInventario("Switch", eletronico2);
+		Armazem<Eletronico> armazem1 = new Armazem<>();
+		armazem1.adicionarAoInventario("TV LG", eletronico1);
+		armazem1.adicionarAoInventario("Switch", eletronico2);
 
-        Eletronico eletronicoBuscado1 = armazem1.obterDoInventario("Switch");
+		Eletronico eletronicoBuscado1 = armazem1.obterDoInventario("Switch");
 
-        System.out.println(eletronicoBuscado1);
+		System.out.println(eletronicoBuscado1);
 
-        Armazem<Comida> armazem2 = new Armazem<>();
-        armazem2.adicionarAoInventario("feijao", comida2);
-        armazem2.adicionarAoInventario("pacoca", comida3);
+		Armazem<Comida> armazem2 = new Armazem<>();
+		armazem2.adicionarAoInventario("feijao", comida2);
+		armazem2.adicionarAoInventario("pacoca", comida3);
 
-        Comida comidaBuscada1 = armazem2.obterDoInventario("feijao");
+		Comida comidaBuscada1 = armazem2.obterDoInventario("feijao");
 
-        System.out.println(comidaBuscada1);
-    }
+		System.out.println(comidaBuscada1);
+	}
 }
