52a0a8977b59a445dbf61edc9f4a37928bf0f283
Lucas Evangelista <73043760+DevDeLucca@users.noreply.github.com>
2022-03-03 08:35:29 -0300

    Java - Generics - Exercícios

--- 0x22/armazem/Eletronico.java
+++ 0x22/armazem/Eletronico.java
@@ -1,36 +1,37 @@
 public class Eletronico {
+	
+	private String descricao;
+	private double valor;
+	
+	public Eletronico() {
+		
+	}
+	
+	public Eletronico(String descricao, double valor) {
+		super();
+		this.descricao = descricao;
+		this.valor = valor;
+	}
 
-    private String descricao;
-    private double valor;
+	public String getDescricao() {
+		return descricao;
+	}
 
-    public comida() {
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 
-    }
+	public double getValor() {
+		return valor;
+	}
 
-    public comida(String descricao, double valor) {
-        this.setDescricao(descricao);
-        this.setValor(valor)
-    }
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	
+	@Override
+	public String toString() {
+		return "["+getDescricao()+"] R$ " +String.format("%f", getValor());
+	}
 
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public double getValor() {
-        return valor;
-    }
-
-    public void setValor(double valor) {
-        this.valor = valor;
-    }
-
-    @override
-    public String toString(){
-        return "["+getDescricao()+"] R$ " +String.format("%f", getValor());
-    }
 }
