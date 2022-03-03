cadc087850d61145c290559947d603c29bde14f8
Lucas Evangelista <73043760+DevDeLucca@users.noreply.github.com>
2022-03-02 21:33:12 -0300

    Java - Generics

--- 0x21/midias/Biblioteca.java
+++ 0x21/midias/Biblioteca.java
@@ -1,15 +1,16 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Biblioteca <T> extends Midia {
+public class Biblioteca<T> extends Midia{
 
-    private List<T> listaValores = new ArrayList<>();
-
-    public void adicionarMidia (T tipo) {
-        listaValores.add(tipo);
-    }
-
-    public  List<T>  obterListaMidias () {
-        return listaValores;
-    }
+	private List<T> listaValores = new ArrayList<>();
+	
+	public void adicionarMidia (T tipo) {
+		listaValores.add(tipo);
+	}
+	
+	public  List<T>  obterListaMidias () {
+		return listaValores;
+	}
+	
 }
