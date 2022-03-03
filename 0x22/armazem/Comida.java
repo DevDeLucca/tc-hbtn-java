7e6b3626a6bb794ce71f7f039f42feee0b165e09
Lucas Evangelista <73043760+DevDeLucca@users.noreply.github.com>
2022-03-03 08:33:55 -0300

    Java - Generics - Exercícios

--- 0x22/armazem/Comida.java
+++ 0x22/armazem/Comida.java
@@ -1,46 +1,45 @@
 public class Comida {
 
-    private String nome;
-    private double calorias;
-    private double preco;
-
-    public comida(){
-
-    }
-
-    public comida(String nome, double calorias, double preco) {
-        this.Nome(nome);
-        this.Calorias(calorias);
-        this.Preco(preco);
-    }
-
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public double getCalorias() {
-        return calorias;
-    }
-
-    public void setCalorias(double calorias) {
-        this.calorias = calorias;
-    }
-
-    public double getPreco() {
-        return preco;
-    }
-
-    public void setPreco(double preco) {
-        this.preco = preco;
-    }
-
-    @Override
-    public String toString() {
-        return "["+getNome()+"] "+ String.format("%f ", getCalorias()) + String.format("R$ %f", getPreco());
-    }
+		private String nome;
+	private double calorias;
+	private double preco;
+	
+	public Comida() {
+		
+	}
+	
+		
+	public Comida(String nome, double calorias, double preco) {
+		super();
+		this.nome = nome;
+		this.calorias = calorias;
+		this.preco = preco;
+	}
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getCalorias() {
+		return calorias;
+	}
+	public void setCalorias(int calorias) {
+		this.calorias = calorias;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "["+getNome()+"] "+ String.format("%f ", getCalorias()) + String.format("R$ %f", getPreco());
+	}
+	
 }
