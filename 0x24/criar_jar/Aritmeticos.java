39d09ee8272d0ce26b2aa85d7673fab3f001ce72
Lucas Evangelista <73043760+DevDeLucca@users.noreply.github.com>
2022-03-07 10:31:40 -0300

    Java - Pacotes JAR

---
+++ 0x24/criar_jar/Aritmeticos.java
@@ -0,0 +1,18 @@
+public class Aritmeticos {
+
+    public static int somar (int valor1, int valor2) {
+        return valor1+valor2;
+    }
+
+    public static int subtrair (int valor1, int valor2) {
+        return valor1-valor2;
+    }
+
+    public static int multiplicar (int valor1, int valor2) {
+        return valor1*valor2;
+    }
+
+    public static int dividir(int valor1, int valor2) {
+        return valor1/valor2;
+    }
+}
